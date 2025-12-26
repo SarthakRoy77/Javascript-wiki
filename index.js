79e7b9583ddfcac1df1a12b4c8fcfb37e44857a1
Sarthak Roy <p-2066-19@dpspatna.com>
2025-12-26 10:21:01 +0530

    add Javascript Basics

---
+++ index.js
@@ -0,0 +1,14 @@
+// Javascript Basics
+console.log("Hello");
+console.log("I like Pizza!")
+// This is a comment
+window.alert("I am Sarthak Roy!");
+/* 
+This 
+is 
+also 
+a 
+comment
+*/ 
+
+document.getElementById("myH1").textContent = "Hello";
