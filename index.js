37bc94a9f00bd7c6a8b41eac02253159d27f4f9d
Sarthak Roy <p-2066-19@dpspatna.com>
2025-12-27 15:16:19 +0530

    add Variables

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ also
 a 
 comment
 */ 
-
 document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza";
---
+++ variables.js
@@ -0,0 +1,36 @@
+
+// Variable = A container that stores a value.Behavaes as if it were the value it contains
+// 1.declaration     let x;
+// 2.assignment      x = 100;
+
+
+// Declaration and assignment as different
+let x;
+x = 100;
+console.log(x);
+
+// Declaration and assignment as one
+let y = 200;
+console.log(y)
+
+// Include variables in outputs 
+let age = 3;
+console.log(`Your age is ${age}`);
+
+//Reveal datatype of a variable
+console.log(typeof age);
+
+//Booleans in Javascript
+let online = true;
+console.log(`bro is online : ${online}`);
+
+//Mini program
+
+let fullName = "Sarthak";
+let age1 = 25; 
+let isStudent = true;
+
+document.getElementById('p1').textContent=`My name is ${fullName}`
+document.getElementById('p2').textContent=`My age is ${age1}`
+document.getElementById('p3').textContent=`Student status : ${isStudent}`
+ 
