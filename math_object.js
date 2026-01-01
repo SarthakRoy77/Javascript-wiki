5b0aad3d53f380d66c470fb59443f1c7f1a26315
Sarthak Roy <p-2066-19@dpspatna.com>
2026-01-01 16:52:30 +0530

    add about Math object

---
+++ math_object.js
@@ -0,0 +1,21 @@
+// Math = built-in-object that provides
+//        a collection of properties 
+//        and methods.
+
+let x = -3.00;
+let y= 2;
+let z;
+z = Math.round(x);//Just rounds
+console.log(Math.floor(x));//Rounds up
+console.log(z);
+console.log(Math.ceil(x));
+console.log(Math.trunc(x)); // Removes the decimals
+console.log(Math.pow(y,x)); // Raises y to x
+console.log(Math.sqrt(x)); // Square root function
+console.log(Math.abs(x)); // asolute function
+console.log(Math.sign(x)); // finds the sign of x
+console.log(Math.max(x,y,z)); //finds the maximum
+console.log(Math.min(x,y,z)); //finds the minimum
+console.log(Math.PI);//outputs pie
+   
+//and many more!!!
