966eb5fc1cc1ae0cfe3102b8a5322690c0e7d715
Sarthak Roy <p-2066-19@dpspatna.com>
2026-01-01 16:30:31 +0530

    Complete Counter Project

--- counter.js
+++ counter.js
@@ -1,3 +1,14 @@
 // Project1 !!!!!!!
 // Counter Program
 
+number = 0;
+
+decreaseBtn = document.getElementById('decreaseBtn').onclick = function() {
+    document.getElementById('countLabel').textContent = number -= 1;
+}
+increaseBtn = document.getElementById('increaseBtn').onclick = function() {
+    document.getElementById('countLabel').textContent = number += 1;
+}
+resetBtn = document.getElementById('resetBtn').onclick = function() {
+    document.getElementById('countLabel').textContent = 0;
+}
