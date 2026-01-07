c2be1b14942f25f3dc72caf17156c99324b266f6
Sarthak Roy <p-2066-19@dpspatna.com>
2026-01-07 16:44:23 +0530

    add about Switch operator

--- if_statements.js
+++ if_statements.js
@@ -2,27 +2,57 @@
             //     if not , do something else
 
 // If else statements
-let age = 25;
+// let age = 25;
 
-if (age>=18) {
-    console.log("You are eligible to vote");
-}
-else{
-    console.log("You are not eligible to vote");
-}
+// if (age>=18) {
+    // console.log("You are eligible to vote");
+// }
+// else{
+    // console.log("You are not eligible to vote");
+// }
 
 // NESTED if else statements
-let age1 = Number(window.prompt("What is your age? "));
-let hasLicense = true
+// let age1 = Number(window.prompt("What is your age? "));
+// let hasLicense = true
 
-if (hasLicense) {
-    if (age1>=18) {
-        console.log("You can drive");
+// if (hasLicense) {
+    // if (age1>=18) {
+        // console.log("You can drive");
+    // }
+    // else {
+        // console.log("Liar!");
+    // }
+// }
+// else {
+    // console.log("You should first have a license!")
+// }
+
+// SWITCH = can be effecient replacement to many else-if statements
+
+let day = 1;
+
+switch(day){
+    case 1:
+        document.getElementById('outputPresenter').textContent = "It is Monday";
+        break;
+    case 2:
+        document.getElementById('outputPresenter').textContent = "It is Tuesday";
+        break;
+    case 3:
+        document.getElementById('outputPresenter').textContent = "It is Wednesday";
+        break;
+    case 4:
+        document.getElementById('outputPresenter').textContent = "It is Thursday";
+        break;
+    case 5:
+        document.getElementById('outputPresenter').textContent = "It is Friday";
+        break;
+    case 6:
+        document.getElementById('outputPresenter').textContent = "It is Saturday";
+        break;
+    case 7:
+        document.getElementById('outputPresenter').textContent = "It is Sunday";
+        break;
+    default:
+        document.getElementById('outputPresenter').textContent = `${day} is not a day`
     }
-    else {
-        console.log("Liar!");
-    }
-}
-else {
-    console.log("You should first have a license!")
-}
