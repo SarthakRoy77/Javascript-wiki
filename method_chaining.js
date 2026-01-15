78560998e3dbcf1ad1dce3363285c7749c6e65f9
Sarthak Roy <p-2066-19@dpspatna.com>
2026-01-15 16:18:39 +0530

    Add on Method Chaining

---
+++ method_chaining.js
@@ -0,0 +1,20 @@
+//Method Chaining = Calling one method after another in one continuous
+//                    line of code
+
+let userName = window.prompt("Enter your username: ");
+
+// No Method Chaining
+userName = userName.trim();
+let letter = userName.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = userName.slice(1);
+extraChars = extraChars.toLowerCase();
+userName = letter + extraChars;
+
+console.log(userName);
+
+// Method Chaining
+userName = userName.trim().charAt(0).toUpperCase().slice(1);
+console.log(userName);
+
