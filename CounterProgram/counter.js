2fbc46b69f7266dcccf523863fe6ef0b75add4c9
Sarthak Roy <p-2066-19@dpspatna.com>
2026-01-18 21:05:05 +0530

    Add JSDOM via npm and add console messages

--- CounterProgram/counter.js
+++ CounterProgram/counter.js
@@ -1,14 +1,18 @@
 // Project1 !!!!!!!
 // Counter Program
 
-number = 0;
+
+let number = 0;
 
 decreaseBtn = document.getElementById('decreaseBtn').onclick = function() {
-    document.getElementById('countLabel').textContent = number -= 1;
+    document.getElementById('countLabel').textContent = ` ${number -= 1} `;
+    console.log("Number subtracted");
 }
 increaseBtn = document.getElementById('increaseBtn').onclick = function() {
-    document.getElementById('countLabel').textContent = number += 1;
+    document.getElementById('countLabel').textContent = `${number += 1}`;
+    console.log("Number added");
 }
 resetBtn = document.getElementById('resetBtn').onclick = function() {
-    document.getElementById('countLabel').textContent = number = 0;
+    document.getElementById('countLabel').textContent = `${number = 0}`;
+    console.log("Number reset");
 }
