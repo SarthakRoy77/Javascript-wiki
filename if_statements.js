bc3c0045500b7a2b9191a5359b9186f474826fee
Sarthak Roy <p-2066-19@dpspatna.com>
2026-02-10 19:24:30 +0530

    add on Rest statements

--- if_statements.js
+++ if_statements.js
@@ -9,11 +9,11 @@ if (age>=18) {
 }
 else{
     console.log("You are not eligible to vote");
-}
+};
  
 // NESTED if else statements
 let age1 = Number(window.prompt("What is your age? "));
-let hasLicense = true
+let hasLicense = true;
 
 if (hasLicense) {
     if (age1>=18) {
@@ -24,10 +24,10 @@ if (hasLicense) {
     }
 }
 else {
-    console.log("You should first have a license!")
+    console.log("You should first have a license!");
 }
 
-// SWITCH = can be effecient replacement to many else-if statements
+// SWITCH = can be efficient replacement to many else-if statements
 
 let day = 1;
 
@@ -55,4 +55,21 @@ switch(day){
         break;
     default:
         document.getElementById('outputPresenter').textContent = `${day} is not a day`
-    }
+    };
+
+// rest parameters = ...rest allow a function work with a variable
+//                       number of arguments by bundling them into an array
+
+//                       spread = expands an array into separate elements
+//                       rest = bundles separate elements into an array
+
+function openFridge(...foods){
+    console.log(...foods);
+};
+
+const food1 = "Pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+
+openFridge(food1,food2,food3,food4);
