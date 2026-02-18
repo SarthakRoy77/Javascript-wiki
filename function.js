470dfb02514cbd84f8806ae831cc12b9a1ffa578
Sarthak Roy <p-2066-19@dpspatna.com>
2026-02-18 19:48:32 +0530

    add on Callbacks

--- function.js
+++ function.js
@@ -24,3 +24,30 @@ function subtract(x,y){
 
 console.log(add(2,3))
 console.log(subtract(2,3))
+
+//callback = a function that is passed as an argument to
+//            another function
+
+//           used to handle asynchronous operations
+//           1. Reading a file
+//           2. Network Requests
+//           3. Interacting with databases
+//          "Hey when you're done , call this next"
+
+// Asynchronous operations in JS are tasks that run in the background without
+// blocking the main execution thread , allowing the program to remain responsive and handle
+// other events concurrently.
+
+hello(goodbye);
+
+function hello(callback) {
+    console.log("hello");
+    callback();
+}
+
+function goodbye() {
+    console.log("goodbye");
+}
+
+// setTimeout function: first functionRef then lines of code and timeout
+// time in Milliseconds
